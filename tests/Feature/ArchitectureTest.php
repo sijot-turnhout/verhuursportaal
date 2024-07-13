2c7dbfc92dc42a5d6d7a578b1dbdc3dd04e23602
Tjoosten <Tjoosten@users.noreply.github.com>
2024-07-13 22:56:21 +0000

    Fix styling

--- tests/Feature/ArchitectureTest.php
+++ tests/Feature/ArchitectureTest.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 arch('Request validation classes must be final')
     ->expect('App\Http\Requests')
     ->classes()
     ->toBeFinal();
 
-arch ('The code in the app directory has strict_types declared')
+arch('The code in the app directory has strict_types declared')
     ->expect('App')
     ->toUseStrictTypes();
