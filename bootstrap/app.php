685d14825507738623438651bc451dcfe1487cc1
Tjoosten <Topairy@gmail.com>
2024-07-13 17:04:25 +0200

    Integreer sentry error monitorinh

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,5 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/statuse',
     )
-    ->withMiddleware(function (Middleware $middleware): void {})
-    ->withExceptions(function (Exceptions $exceptions): void {})->create();
+    ->withMiddleware(function (Middleware $middleware): void {
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        Integration::handles($exceptions);
+    })->create();
