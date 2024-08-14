e547ba5045d533f9d53e7a17645eb4eafa1e8253
Tjoosten <Tjoosten@users.noreply.github.com>
2024-08-14 19:53:10 +0000

    Fix styling

--- tests/Feature/LeaseResource/States/NewLeaseRequestStateTest.php
+++ tests/Feature/LeaseResource/States/NewLeaseRequestStateTest.php
@@ -10,21 +10,17 @@ beforeEach(function (): void {
     $this->lease = Lease::factory()->create();
 });
 
-it ('can mark the leasqe request as a quotation option', function (): void {
-});
+it('can mark the leasqe request as a quotation option', function (): void {});
 
 
-it ('can mark the lease with the option state', function (): void {
-});
+it('can mark the lease with the option state', function (): void {});
 
-it ('can mark the lease with the cancelled state', function (): void {
-});
+it('can mark the lease with the cancelled state', function (): void {});
 
-it ('cannot mark the lease with the finalized state', function (): void {
+it('cannot mark the lease with the finalized state', function (): void {
     $this->lease->state()->transitionToFinalized();
 })->throws('Cannot transition to the Finalized state on the current state.');
 
-it ('cannot mark the lease with the confirmed state', function (): void {
+it('cannot mark the lease with the confirmed state', function (): void {
     $this->lease->state()->transitionToConfirmed();
 })->throws('Cannot transition to the Confirmed state with the current state.');
-
