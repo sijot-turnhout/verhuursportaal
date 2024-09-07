68bed33423f25f88411ceb92c24574395648bed8
Tim Joosten <Tjoosten@users.noreply.github.com>
2024-09-07 20:52:10 +0200

    docs: Add detailed documentation to the Lease Pages

--- app/Filament/Resources/LeaseResource/Pages/EditLease.php
+++ app/Filament/Resources/LeaseResource/Pages/EditLease.php
@@ -9,10 +9,35 @@ use App\Filament\Resources\LeaseResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
-class EditLease extends EditRecord
+/**
+ * Class EditLease
+ *
+ * The `EditLease` class is responsible for providing the functionality to edit lease records
+ * within the system. It extends the `EditRecord` class to allow modifications to lease details
+ * and integrates actions related to invoices, such as generating and viewing invoices.
+ *
+ * @package App\Filament\Resources\LeaseResource\Pages
+ */
+final class EditLease extends EditRecord
 {
-    protected static string $resource = LeaseResource::class;
+    /**
+     * The associated resource for the edit page.
+     *
+     * This property links the `EditLease` page to the `LeaseResource` class, which defines
+     * the schema and behavior for managing lease records.
+     *
+     * @var string
+     */    protected static string $resource = LeaseResource::class;
 
+    /**
+     * Get the header actions available on the edit page.
+     *
+     * This method returns an array of actions that are displayed in the header of the edit
+     * page. It includes actions for generating and viewing invoices, as well as a delete action
+     * for removing the lease record.
+     *
+     * @return array An array of actions for the edit page header.
+     */
     protected function getHeaderActions(): array
     {
         return [
