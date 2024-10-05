3b7b2b593b4347e002aadc0d57bfc28ae8e31535
Tim Joosten <Tjoosten@users.noreply.github.com>
2024-10-06 01:57:39 +0200

    Sync laravel boilerplate code to v11.2.0

--- config/filesystems.php
+++ config/filesystems.php
@@ -26,7 +26,7 @@ return [
     | may even configure multiple disks for the same driver. Examples for
     | most supported storage drivers are configured here for reference.
     |
-    | Supported Drivers: "local", "ftp", "sftp", "s3"
+    | Supported drivers: "local", "ftp", "sftp", "s3"
     |
     */
 
@@ -34,7 +34,8 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => storage_path('app/private'),
+            'serve' => true,
             'throw' => false,
         ],
 
