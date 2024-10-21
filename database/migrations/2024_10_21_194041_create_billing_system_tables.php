8653559c18fc8145ff7195d0d93fa576784c3d11
Tim Joosten <Tjoosten@users.noreply.github.com>
2024-10-21 22:06:25 +0200

    Squash billing system database migrations

--- database/migrations/2024_10_21_194041_create_billing_system_tables.php
+++ database/migrations/2024_10_21_194041_create_billing_system_tables.php
@@ -1,21 +1,35 @@
 <?php
 
-declare(strict_types=1);
-
 use App\Models\Invoice;
-use App\Models\Lease;
-use App\Models\Tenant;
+use App\Models\Quotation;
 use App\Models\User;
+use App\Models\Tenant;
+use App\Models\Lease;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
+        Schema::create('quotations', static function (Blueprint $table) {
+            $table->id();
+            $table->string('reference');
+            $table->string('status');
+            $table->foreignIdFor(User::class, 'user_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignIdFor(Lease::class, 'lease_id')->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Tenant::class, 'reciever_id')->nullable()->references('id')->on('tenants')->nullOnDelete();
+            $table->text('description')->nullable();
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamp('approved_at')->nullable();
+            $table->timestamp('rejected_at')->nullable();
+            $table->timestamps();
+        });
+
         Schema::create('invoices', function (Blueprint $table): void {
             $table->id();
             $table->string('payment_reference')->unique()->comment('Factuur referentie');
@@ -31,6 +45,7 @@ return new class () extends Migration {
         });
 
         Schema::table('leases', function (Blueprint $table): void {
+            $table->foreignIdFor(Quotation::class)->nullable()->references('id')->on('quotations')->cascadeOnDelete();
             $table->foreignIdFor(Invoice::class)->nullable()->references('id')->on('invoices')->cascadeOnDelete();
         });
     }
@@ -40,6 +55,13 @@ return new class () extends Migration {
      */
     public function down(): void
     {
+        Schema::table('leases', function (Blueprint $table): void {
+            $table->dropForeignIdFor(Quotation::class);
+            $table->dropForeignIdFor(Invoice::class);
+        });
+
+        Schema::dropIfExists('billing_items');
         Schema::dropIfExists('invoices');
+        Schema::dropIfExists('quotations');
     }
 };
