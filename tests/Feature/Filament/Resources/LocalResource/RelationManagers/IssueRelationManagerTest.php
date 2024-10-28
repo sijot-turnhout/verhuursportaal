dee8067bd7a7bf45e5a193c9ca89298229315fab
Tim Joosten <Tjoosten@users.noreply.github.com>
2024-10-28 04:50:31 +0100

    Add tests for the local relation manager

---
+++ tests/Feature/Filament/Resources/LocalResource/RelationManagers/IssueRelationManagerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+describe('Issue relation manager tests', function (): void {
+    it('can display the issue relation manager', function (): void {});
+    it('can sort columns', function (): void {});
+    it('can render columns', function (): void {});
+    it('can search columns', function (): void {});
+})->skip();
