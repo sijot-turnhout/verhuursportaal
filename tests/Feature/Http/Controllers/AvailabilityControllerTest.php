bb125ab1d653c0a4af399e5529a8dfd65deeecc7
Tim Joosten <Tjoosten@users.noreply.github.com>
2024-11-12 20:56:13 +0100

    Add availability controller tests when leases are stored

--- tests/Feature/Http/Controllers/AvailabilityControllerTest.php
+++ tests/Feature/Http/Controllers/AvailabilityControllerTest.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Http\Controllers;
 
-it('can successfully display the availability page', function (): void {
+use App\Models\Lease;
+
+it('can successfully display the availability page when no leases are stored', function (): void {
+    $this->get(route('availability'))->assertSuccessful();
+});
+
+it('can successfully display the availability page when leases are stored', function (): void {
+    Lease::factory(25)->create();
     $this->get(route('availability'))->assertSuccessful();
 });
