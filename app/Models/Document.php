0eee2b5a5665fd5c70f949f451039381052acd01
Tim Joosten <Tjoosten@users.noreply.github.com>
2024-12-14 02:28:44 +0100

    Document the properties in the document model

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -15,6 +15,14 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * This model is observed by the DocumentObserver for lifecycle events
  * and maintains a relationship to the User model, identifying the document creator.
  *
+ * @property int                              $id          The unique identifier from the document in the database storage.
+ * @property int                              $user_id     The unique identifier from the user who saved the document in the application.
+ * @property int                              $lease_id    The unique identifier for the associated lease with the document.
+ * @property string                           $name        The name of the document that has been stored.
+ * @property string                           $attachment  The file path of the document that has been stored.
+ * @property \Illuminate\Support\Carbon|null  $created_at  The timestamp indicating when the document has been stored in the application.
+ * @property \Illuminate\Support\Carbon|null  $updated_at  The Timestamp indicating when the document has been updated for the last time.
+ *
  * @package App\Models
  */
 #[ObservedBy(DocumentObserver::class)]
