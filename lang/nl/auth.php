af026760162962c3626f3205a2dfb269051aa0cb
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-01-06 15:09:54 +0100

    Implement the dutch translations for the auth system

---
+++ lang/nl/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Deze combinatie van e-mailadres en wachtwoord is niet geldig.',
+    'password' => 'Het gegeven wachtwoord is onjuist.',
+    'throttle' => 'Te veel mislukte aanmeldpogingen. Probeer het het nog een over :seconds seconden.',
+
+];
