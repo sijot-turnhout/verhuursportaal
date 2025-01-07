5e326924065d7873b3e84564cedd44c271a18f2c
Tim Joosten <Tjoosten@users.noreply.github.com>
2025-01-07 20:08:10 +0100

    Implement the dutch translations for the password system

---
+++ lang/nl/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Het wachtwoord van uw account is gewijzigd.',
+    'sent' => 'We hebben een e-mail vestuurd met instructies om een nieuw wachtwoord in te stellen.',
+    'throttled' => 'Gelieve even te wachten avorens het opniew te proberen.',
+    'token' => 'Deze wachtwoord herstel token is ongeldig.',
+    'user' => "Geenj gebruiker bekend met het opgegeven e-mailadres.",
+
+];
